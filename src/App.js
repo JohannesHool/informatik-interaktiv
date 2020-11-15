5a0bc47389e0e2372271a17fc813c3deb77aa514
JohannesHool <hooljohannes@gmail.com>
2020-11-15 16:21:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+
+let PAGES = [
+  ["Zahlensysteme", ""],
+  ["Zeichenkodierung", "https://johanneshool.github.io/zeichenKonverter/"],
+  ["Pixelbilder", "https://johanneshool.github.io/binaerbilder/"]
+]
+
+function App() {
+
+  function createList(dict) {
+    let topics = [];
+
+    for (let i = 0; i < PAGES.length; i++) {
+      topics.push(
+        <div className="entry">
+          <a href={PAGES[i][1]} class="btn btn-secondary btn-lg btn-block" role="button" aria-disabled="true">{PAGES[i][0]}</a>
+        </div>
+      );
+    }
+
+
+
+    return (
+      <div>
+        {topics}
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-6 center-bar">
+          <div className="text-center title-container rounded">
+            <h1>Informatik Interaktiv</h1>
+          </div>
+          {createList(PAGES)}
+          <div className="footer">
+            <p>Contact: <a href="mailto:hooljohannes@gmail.com">Johannes Hool</a></p>
+          </div>
+        </div>
+        <div className="col-md-2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
